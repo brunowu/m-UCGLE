1980cb0fb0607e6966beca286878ff1011d66ef5
brunowu <xinzhe.wu1990@gmail.com>
2018-06-05 11:58:55 +0200

    start basic spawn implementation

---
+++ arnoldi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main( int argc, char *argv[] ){
+  MPI_Init( &argc, &argv );
+
+  int grank, gsize;
+
+  MPI_Comm_size( MPI_COMM_WORLD, &gsize );
+  MPI_Comm_rank( MPI_COMM_WORLD, &grank );
+
+  if(grank == 0){
+    printf("Info ]> The Comm world size of ERAM is %d \n", gsize);
+  }
+
+
+  MPI_Finalize();
+
+  return 0;
+
+}
---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main( int argc, char *argv[] ) {
+
+  MPI_Init( &argc, &argv );
+
+  int size, rank;
+  MPI_Comm_size( MPI_COMM_WORLD, &size );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+  if(rank == 0){
+    printf("Info ]> The Comm world size of FATHER is %d \n", size);
+  }
+
+  int GMRES_SIZE = 2, ARNOLDI_SIZE = 2, LS_SIZE = 1;
+
+  MPI_Comm COMM_GMRES, COMM_ARNOLDI, COMM_LS;
+
+  MPI_Comm_spawn( "./gmres.exe", MPI_ARGV_NULL, GMRES_SIZE, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &COMM_GMRES, MPI_ERRCODES_IGNORE);
+  MPI_Comm_spawn( "./arnoldi.exe", MPI_ARGV_NULL, ARNOLDI_SIZE, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &COMM_ARNOLDI, MPI_ERRCODES_IGNORE);
+  MPI_Comm_spawn( "./lsqr.exe", MPI_ARGV_NULL, LS_SIZE, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &COMM_LS, MPI_ERRCODES_IGNORE);
+
+  MPI_Finalize();
+
+  return 0;
+}
