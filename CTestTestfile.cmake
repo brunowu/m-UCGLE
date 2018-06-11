42ba65184cbe0ab2728d5f7099e60b311f7d081b
brunowu <xinzhe.wu1990@gmail.com>
2018-06-11 09:39:38 +0200

    add initially cmake for compilation

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xwu/phd_xwu/UCMGEL
+# Build directory: /Users/xwu/phd_xwu/UCMGEL
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(Solvers)
---
+++ Solvers/Arnoldi/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/xwu/phd_xwu/UCMGEL/Solvers/Arnoldi
+# Build directory: /Users/xwu/phd_xwu/UCMGEL/Solvers/Arnoldi
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
