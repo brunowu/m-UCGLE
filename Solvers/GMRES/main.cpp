0095413b15dbe3f150fc36b3bf0a8d6a65001f40
brunowu <xinzhe.wu1990@gmail.com>
2018-07-24 18:15:35 +0200

    basic gmres works sans ls

--- Solvers/GMRES/main.cpp
+++ Solvers/GMRES/main.cpp
@@ -14,190 +14,218 @@
 #include "BelosConfigDefs.hpp"
 #include "BelosLinearProblem.hpp"
 #include "BelosTpetraAdapter.hpp"
+#include "BelosBlockGmresLsSolMgr.hpp"
 
 // I/O for Matrix-Market files
 #include <MatrixMarket_Tpetra.hpp>
 #include <Tpetra_Import.hpp>
 
-int main(int argc, char *argv[]) {
-  //Tpetra::ScopeGuard tpetraScope(&argc,&argv);
-  Teuchos::GlobalMPISession mpisess (&argc, &argv, &std::cout);
+int main(int argc, char *argv[]){
+//Tpetra::ScopeGuard tpetraScope(&argc,&argv);
+	Teuchos::GlobalMPISession mpisess (&argc, &argv, &std::cout);
 
-  typedef double Scalar;
-  typedef Teuchos::ScalarTraits<Scalar>         SCT;
-  typedef SCT::magnitudeType                  MT;
+  	typedef double 									Scalar;
+  	typedef Teuchos::ScalarTraits<Scalar>         	SCT;
+  	typedef SCT::magnitudeType                  	MT;
 
-  typedef Tpetra::Map<>::local_ordinal_type LO;
-  typedef Tpetra::Map<>::global_ordinal_type GO;
+  	typedef Tpetra::Map<>::local_ordinal_type 		LO;
+  	typedef Tpetra::Map<>::global_ordinal_type 		GO;
+	typedef Tpetra::Operator<Scalar,int>         	OP;
+  	typedef Tpetra::CrsMatrix<Scalar,LO,GO> 		MAT;
+  	typedef Tpetra::MultiVector<Scalar,LO,GO> 		MV;
+  	typedef Belos::MultiVecTraits<Scalar,MV>    	MVT;
+  	typedef Belos::OperatorTraits<Scalar,MV,OP> 	OPT;
 
-  typedef Tpetra::CrsMatrix<Scalar,LO,GO> MAT;
-  typedef Tpetra::MultiVector<Scalar,LO,GO> MV;
-  typedef Belos::MultiVecTraits<Scalar,MV>    MVT;
+  	const Scalar ONE  = SCT::one();
 
-  const Scalar ONE  = SCT::one();
+  	using Tpetra::global_size_t;
+  	using Tpetra::Map;
+  	using Tpetra::Import;
+  	using Teuchos::RCP;
+  	using Teuchos::rcp;
 
-  using Tpetra::global_size_t;
-  using Tpetra::Map;
-  using Tpetra::Import;
-  using Teuchos::RCP;
-  using Teuchos::rcp;
+  	//
+  	// Get the default communicator
+  	//
+  	Teuchos::RCP<const Teuchos::Comm<int> > comm = Tpetra::getDefaultComm();
+  	int myRank = comm->getRank();
 
+  	Teuchos::oblackholestream blackhole;
 
-  //
-  // Get the default communicator
-  //
-  Teuchos::RCP<const Teuchos::Comm<int> > comm = Tpetra::getDefaultComm();
-  int myRank = comm->getRank();
+  	bool printMatrix   = false;
+  	bool allprint      = false;
+  	bool verbose 	   = (myRank==0);
+  	bool debug 		   = false;
+  	std::string filename("utm300.mtx");
+  	int frequency 	   = -1;
+  	int numVectors 	   = 1;
+	int blocksize 	   = 100;
+  	int numblocks 	   = 5;
+  	double tol 		   = 1.0e-5;
+  	bool precond 	   = false;
+  	bool dumpdata 	   = false;
+  	bool reduce_tol;
+  	Teuchos::ParameterList mptestpl;
+  	Belos::ReturnType ret;
+  	double t1, t2, t3, t4, t5, t6;
 
-  Teuchos::oblackholestream blackhole;
+  	Teuchos::CommandLineProcessor cmdp(false,true);
+  	cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
+  	cmdp.setOption("debug","nodebug",&debug,"Run debugging checks.");
+  	cmdp.setOption("frequency",&frequency,"Solvers frequency for printing residuals (#iters).");
+ 	cmdp.setOption("tol",&tol,"Relative residual tolerance used by solver.");
+ 	cmdp.setOption("num-rhs",&numVectors,"Number of right-hand sides to be solved for.");
+ 	cmdp.setOption("block-size",&blocksize,"Block size to be used by the solver.");
+ 	cmdp.setOption("use-precond","no-precond",&precond,"Use a diagonal preconditioner.");
+	cmdp.setOption("num-blocks",&numblocks,"Number of blocks in the Krylov basis.");
+	cmdp.setOption("reduce-tol","fixed-tol",&reduce_tol,"Require increased accuracy from higher precision scalar types.");
+  	cmdp.setOption("filename",&filename,"Filename for Matrix-Market test matrix.");
+  	cmdp.setOption("print-matrix","no-print-matrix",&printMatrix,"Print the full matrix after reading it.");
+  	cmdp.setOption("all-print","root-print",&allprint,"All processors print to out");
+  	cmdp.setOption("dump-data","no-dump-data",&dumpdata,"Dump raw data to data.dat.");
 
-  bool printMatrix   = false;
-  bool allprint      = false;
-  bool verbose = (myRank==0);
-  std::string filename("arc130.mtx");
-  Teuchos::CommandLineProcessor cmdp(false,true);
-  cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
-  cmdp.setOption("filename",&filename,"Filename for Matrix-Market test matrix.");
-  cmdp.setOption("print-matrix","no-print-matrix",&printMatrix,"Print the full matrix after reading it.");
-  cmdp.setOption("all-print","root-print",&allprint,"All processors print to out");
-  if (cmdp.parse(argc,argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL) {
-    return -1;
-  }
+  	if (cmdp.parse(argc,argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL) {
+    	return -1;
+  	}
 
-  std::ostream& out = ( (allprint || (myRank == 0)) ? std::cout : blackhole );
-  RCP<Teuchos::FancyOStream> fos = Teuchos::fancyOStream(Teuchos::rcpFromRef(out));
+  	if (debug) {
+    	verbose = true;
+  	}
 
-  const size_t numVectors = 1;
+  	if (!verbose) {
+    	frequency = -1;
+  	}
 
-  RCP<MAT> A = Tpetra::MatrixMarket::Reader<MAT>::readSparseFile(filename, comm);
-  if( printMatrix ){
-    A->describe(*fos, Teuchos::VERB_EXTREME);
-  }
-  else if( verbose ){
-    std::cout << std::endl << A->description() << std::endl << std::endl;
-  }
+	mptestpl.set( "Block Size", blocksize );
+ 	mptestpl.set( "Num Blocks", numblocks);
 
-  // get the maps
-  RCP<const Map<LO,GO> > dmnmap = A->getDomainMap();
-  RCP<const Map<LO,GO> > rngmap = A->getRangeMap();
-
-  GO nrows = dmnmap->getGlobalNumElements();
-  RCP<Map<LO,GO> > root_map
-    = rcp( new Map<LO,GO>(nrows,myRank == 0 ? nrows : 0,0,comm) );
-  RCP<MV> Xhat = rcp( new MV(root_map,numVectors) );
-  RCP<Import<LO,GO> > importer = rcp( new Import<LO,GO>(dmnmap,root_map) );
-
-  // Create random X
-  RCP<MV> X = rcp(new MV(dmnmap,numVectors));
-  X->randomize();
-  RCP<MV> vec_sol = MVT::Clone(*X,numVectors);
-  vec_sol->putScalar(0.0);
-
-  RCP<MV> x_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> r0_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> w_1_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> w1_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> w0_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> r1_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> sol_tmp = MVT::Clone(*vec_sol,numVectors);
-  RCP<MV> vec_tmp = MVT::Clone(*vec_sol,numVectors);
-
-  x_tmp->putScalar(0.0);
-  r0_tmp->putScalar(0.0);
-  w_1_tmp->putScalar(0.0);
-  w1_tmp->putScalar(0.0);
-  w0_tmp->putScalar(0.0);
-  r1_tmp->putScalar(0.0);
-  sol_tmp->putScalar(0.0); //later should be replaced by the current solution in GMRES
-  vec_tmp->putScalar(0.0);
-
-  RCP<MV> vec_rhs = rcp(new MV(rngmap,numVectors));
-  vec_rhs->putScalar(1); //should be replaced by getRHS
-
-  int ls_power = 5;
-
-  int i, j;
-
-  std::vector<MT> normV( numVectors );
-
-  //simulation of the received data from LS
-  double data_tmp [32] = {10.000000,-0.720766,-1.395545,-0.660261,-0.527755,-0.457798,-0.368613,-0.319541,-0.259549,-0.189444,-0.168309
-                        -0.119417,0.326302,0.576631,0.609613,0.611939,0.612093,0.612104,0.612104,0.612104,0.612104,0.612104,0.000000,
-                        0.075973,0.042991,0.040666,0.040511,0.040501,0.040500,0.040500,0.040500,0.040500};
-
-  
-  int size = (int) data_tmp[0];
-  double alpha;
-  double *eta = new double [size];
-  double *beta = new double [size];
-  double *delta = new double [size];
-
-  std::memcpy(&alpha,&data_tmp[1], 1*sizeof(double));
-  std::memcpy(eta,&data_tmp[2], size*sizeof(double));
-  std::memcpy(beta,&data_tmp[2 + size], size*sizeof(double));
-  std::memcpy(delta,&data_tmp[2 + 2 * size], size*sizeof(double));
+  	mptestpl.set<MT>( "Convergence Tolerance", tol );
+  	mptestpl.set( "Timer Label",  Teuchos::typeName(ONE) );
 
 
-  //A->apply(*x_tmp,*r0_tmp);
-  //r0_tmp->describe(*fos, Teuchos::VERB_EXTREME);
+	int verbLevel = Belos::Errors + Belos::Warnings;
+/*
+	if (debug) {
+    	verbLevel |= Debug;
+  	}
+*/
+  	if (verbose) {
+    	verbLevel |= Belos::TimingDetails + Belos::FinalSummary + Belos::StatusTestDetails;
+  	}
+  	mptestpl.set( "Verbosity", verbLevel );
+  	if (verbose) {
+    	if (frequency > 0) {
+      		mptestpl.set( "Output Frequency", frequency );
+    	}
+  	}
 
-  for(j = 0; j < ls_power; j++){
-    /* r0 = b-Ax*/
-    /* put A*x into vec_tmp */
-    A->apply(*sol_tmp,*vec_tmp);
-    MVT::MvAddMv( -ONE, *vec_tmp, ONE, *vec_rhs, *r0_tmp );
-    w0_tmp = MVT::CloneCopy(*r0_tmp);
-    x_tmp = MVT::CloneCopy(*w0_tmp);
-    MVT::MvScale(*x_tmp, eta[0]);
-    //vec_tmp->describe(*fos, Teuchos::VERB_EXTREME);
-    w_1_tmp->putScalar(0.0);
-    /* depending on the ls polynom size (int)data_tmp[0] = size */
-    for(i = 0; i < size - 1; i++){
-      /* w1=-alpha*w0 - delta[i]*w_1 ((  y = alpha x + delta y. ))*/
-      w1_tmp = MVT::CloneCopy(*w_1_tmp);
-      /*AXPBY*/
-      MVT::MvAddMv( -alpha, *w1_tmp, delta[i], *w0_tmp, *w1_tmp );
-      /* w1 = w1 - A*w0 */
-      A->apply(*w0_tmp,*vec_tmp);
-      /* y = alpha x + y.*/
-      MVT::MvAddMv( ONE, *w1_tmp, -ONE, *vec_tmp, *w1_tmp );
-      /* w1 = w1/beta[i] */
-      MVT::MvScale(*w1_tmp, 1/beta[i]);
-      /* w_1 = w0 */
-      w_1_tmp = MVT::CloneCopy(*w0_tmp);
-      /* w0 = w1*/
-      w0_tmp = MVT::CloneCopy(*w1_tmp);
-      /* x = x + (w0 * eta[i] ) */
-      MVT::MvAddMv( ONE, *x_tmp, eta[i + 1], *w0_tmp, *x_tmp );      
+  	std::ostream& out = ( (allprint || (myRank == 0)) ? std::cout : blackhole );
+  	RCP<Teuchos::FancyOStream> fos = Teuchos::fancyOStream(Teuchos::rcpFromRef(out));
+
+	if (myRank==0){
+		std::cout << "Testing Scalar == " << Teuchos::typeName(ONE) << std::endl;
+	}
+
+  	/*build the problem by loading matrix market from local file systems*/
+  	if (myRank==0){
+  		std::cout << "GMRES ]> Building problem..." << std::endl;
+  	}
+  	
+  	
+	//	Teuchos::TimeMonitor localtimer(btimer); 
+	t1 = MPI_Wtime();
+
+  	RCP<MAT> A = Tpetra::MatrixMarket::Reader<MAT>::readSparseFile(filename, comm);
+  	if(myRank == 0){
+  		std::cout << "GMRES ]> Load matrix: " << filename << " from local filesystem" << std::endl;
+  	}
+
+  	if( printMatrix ){
+    	A->describe(*fos, Teuchos::VERB_EXTREME);
+  	}
+
+  	else if( verbose ){
+    	std::cout << std::endl << A->description() << std::endl << std::endl;
+  	}
+
+  	// get the maps
+  	RCP<const Map<LO,GO> > dmnmap = A->getDomainMap();
+  	RCP<const Map<LO,GO> > rngmap = A->getRangeMap();
+
+  	GO nrows = dmnmap->getGlobalNumElements();
+  	RCP<Map<LO,GO> > root_map
+    	= rcp( new Map<LO,GO>(nrows,myRank == 0 ? nrows : 0,0,comm) );
+  	RCP<MV> Xhat = rcp( new MV(root_map,numVectors) );
+  	RCP<Import<LO,GO> > importer = rcp( new Import<LO,GO>(dmnmap,root_map) );
+
+  	// generate randomly the final solution of systems as X
+  	RCP<MV> X = rcp(new MV(dmnmap,numVectors));
+  	//X->randomize();
+  	MVT::MvRandom(*X);
+  	X->describe(*fos, Teuchos::VERB_EXTREME);
+
+  	/*generate the right hand sides B by given X and A */
+  	RCP<MV> B = rcp(new MV(dmnmap,numVectors));
+  	OPT::Apply( *A, *X, *B );
+  	MVT::MvInit( *X, 0.0 );
+
+	RCP<Belos::LinearProblem<Scalar,MV,OP> > problem = rcp( new Belos::LinearProblem<Scalar,MV,OP>(A,X,B) );
+
+	problem->setLabel(Teuchos::typeName(SCT::one()));
+
+	TEUCHOS_TEST_FOR_EXCEPT(problem->setProblem() == false);
+
+  	t2 = MPI_Wtime();
+
+	if(myRank == 0) printf("Building TIME = %f\n", t2 - t1 ); 
+  	
+	/////////////////////////////////////////////////
+	/*construct GMRES solver*/
+	RCP<Belos::SolverManager<Scalar,MV,OP> > solver;
+	if (myRank==0){
+  		std::cout << "GMRES ]> Construct solver ..." << std::endl;
+  	}
+  	
+  	t3 = MPI_Wtime();
+  	//Teuchos::TimeMonitor localtimer(ctimer); 
+  	solver = rcp(new Belos::BlockGmresSolMgr<Scalar,MV,OP>( problem, rcp(&mptestpl,false) ));
+  	t4 = MPI_Wtime();
+  	
+  	if(myRank == 0) printf("Construct TIME = %f\n", t4 - t3 ); 
+
+  	if(myRank == 0) printf("GMRES ]> Solving problem ...\n" ); 
+
+	int diter;
+
+	t5 = MPI_Wtime();
+	try {
+      ret = solver->solve();
     }
-    /* update solution, x1= x1+x*/
-    MVT::MvAddMv( ONE, *sol_tmp, ONE, *w0_tmp, *sol_tmp );  
-    /* put A*x into VEC_TEMP */
-    A->apply(*sol_tmp,*vec_tmp);   
-    /* now put residual (-A*x + f) into vec_vv(0) */
-    MVT::MvAddMv( -ONE, *vec_tmp, ONE, *vec_rhs, *r1_tmp );  
-    /* compute norm and see if it's below epsilon */
-    MVT::MvNorm( *r1_tmp, normV);
 
-    std::vector<Scalar>::iterator iter_begin = normV.begin();
-    std::vector<Scalar>::iterator iter_end   = normV.end();
-    std::vector<Scalar>::iterator iter;
-
-    for(iter = iter_begin; iter != iter_end; ++iter){
-      std::cout << "LOOP" << j << ": "<< *iter << std::endl;
+    catch (std::exception &e) {
+      std::cout << "Caught exception: " << std::endl << e.what() << std::endl;
+      ret = Belos::Unconverged;
     }
 
-  }
+    t6 = MPI_Wtime();
 
-//  Teuchos::RCP<MV> curX = problem_->getCurrLHSVec();
-//  MVT::MvAddMv( 1.0, *curX, 1.0, *update, *curX );
+	if(myRank == 0) printf("Solving TIME = %f\n", t6 - t5 ); 
 
-  delete [] eta;
-  delete [] beta;
-  delete [] delta;
+	diter = solver->getNumIters();
 
-//  Teuchos::TimeMonitor::summarize();
+	if (ret == Belos::Converged){
+		if(myRank == 0){
+			printf("GMRES ]> This GMRES Component is converged\n");
+		}
+	}
+	else{
+		if(myRank == 0){
+			printf("GMRES ]> This GMRES Component cannot be converged with given parameters\n");
+		}
+	}
 
-  // LS is done.
-  return 0;
+
+	return 0;
+
+
 }
